a9802487bfa0c5a6df5146bfe836223a7cce21ab
raulito28 <raulbuenoherrera@gmail.com>
2021-10-14 19:49:14 +0200

    Update

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ width = int(input("Ancho de la ventana: "))
 height = int(input("Alto de l ventana: "))
 size[0] = width
 size[1] = height
+#Set color background
+colorfondo = [255, 255, 255]
+nivelrojo = int(input("Nivel de color rojo: "))
+nivelverde = int(input("Nivel de color verde: "))
+nivelazul = int(input("Nivel de verde: "))
+colorfondo[0] = nivelrojo
+colorfondo[1] = nivelverde
+colorfondo[2] = nivelazul
 #call main routine
 titulo = input("Inserta un título: ")
-main2(size, titulo)
+main2(size, titulo, colorfondo)
